66910732e3cdf7ae0f50e8c8d5ed6d1bc7f6a5f5
Jake Houghton <jakehoughton@ksu.edu>
2026-03-12 15:55:57 -0500

    fixed webGL vertically flipped video bug

--- src/app/renderer.webgl.js
+++ src/app/renderer.webgl.js
@@ -402,6 +402,7 @@ export class WebGLRenderer {
       default: return;
     }
     gl.bindTexture(gl.TEXTURE_2D, tex);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, source);
   }
 
@@ -419,6 +420,7 @@ export class WebGLRenderer {
       default: return;
     }
     gl.bindTexture(gl.TEXTURE_2D, tex);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, imageData.width, imageData.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, imageData.data);
   }
 
